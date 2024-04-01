f9728142ec569fcc0dd37a31e5873ba10c06ae57
unknown <panditdipesh12@gmail.com>
2024-04-01 11:09:26 +0545

    completed the designing

--- src/components/pages/Home/HomePage.js
+++ src/components/pages/Home/HomePage.js
@@ -8,7 +8,7 @@ export default function HomePage(){
             <Hero/>
             <WeekSpecials/>
             <Testimonials/>
-            {/* <OurStory/> */}
+            <OurStory/>
         </>
     )
 }
--- src/components/pages/Home/TestimonialCard.js
+++ src/components/pages/Home/TestimonialCard.js
@@ -1,7 +1,26 @@
+import {
+    faStar,
+    faStarHalfStroke
+  } from '@fortawesome/free-solid-svg-icons';
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import './TestimonialCard.css';
 
-
-export default function TestimonialCard(){
+  const ratingLevels={'0.5' : faStarHalfStroke, '1':faStar,};
+export default function TestimonialCard({customer}){
     return(
-        <>hlo</>
+        <article className='testimonial-card'>
+            <img src={customer.image} alt={customer.fullName}/>
+            <h4>{customer.fullName}</h4>
+            <span>
+                {customer.rating.map((ratingPoint,index)=>
+                    <FontAwesomeIcon
+                    key={index}
+                    icon={ratingLevels[ratingPoint]}
+                    size='xs'
+                    />
+                )}
+            </span>
+            <blockquote><p>'{customer.says}'</p></blockquote>
+        </article>
     )
 }
